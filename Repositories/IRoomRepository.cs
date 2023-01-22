3a06bcfcc5ecb04b9bc79c950abc99791967b4ad
timigyurina <timi.gyurina@gmail.com>
2023-01-22 10:20:17 +0100

    create interface and implementation for RoomRepository (for functions that are only used with objects of Room type)

---
+++ Repositories/IRoomRepository.cs
@@ -0,0 +1,14 @@
+﻿using HogwartsPotions.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HogwartsPotions.Repositories
+{
+    public interface IRoomRepository : IGenericRepository<Room>
+    {
+        Task<IEnumerable<Room>> GetAvailable();
+        Task<IEnumerable<Room>> GetRoomsOfRatOwners();
+        //Task<Room?> AssignTo(int roomId, Student student);
+        //Task<bool> RemoveStudentFrom(int originalRoomIdOfStudent, Student student);
+        //Task<IEnumerable<Room>> SearchRooms(string? houseType, int? capacity, int? numberOfResidents);
+    }
+}
---
+++ Repositories/RoomRepository.cs
@@ -0,0 +1,22 @@
+﻿using HogwartsPotions.Data;
+using HogwartsPotions.Models.Entities;
+
+namespace HogwartsPotions.Repositories
+{
+    public class RoomRepository : GenericRepository<Room>, IRoomRepository
+    {
+        public RoomRepository(HogwartsContext context) : base(context)
+        {
+        }
+
+        public Task<IEnumerable<Room>> GetAvailable()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Room>> GetRoomsOfRatOwners()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
