e38bc66c8ad0fb16a9ab56ffeada9fe28ec19bc4
timigyurina <timi.gyurina@gmail.com>
2023-01-22 20:35:21 +0100

    create interface and implementation for StudentRepository

---
+++ Repositories/IStudentRepository.cs
@@ -0,0 +1,12 @@
+﻿using HogwartsPotions.Models.Entities;
+
+namespace HogwartsPotions.Repositories
+{
+    public interface IStudentRepository : IGenericRepository<Student>
+    {
+        Task<Student?> AssignRoomTo(int id, int roomId);
+        Task<Student?> GetWithDetails(int id);
+       
+        //Task<IEnumerable<Student>> SearchStudents(string? houseType, string? petType, int? roomId);
+    }
+}
---
+++ Repositories/StudentRepository.cs
@@ -0,0 +1,33 @@
+﻿using HogwartsPotions.Data;
+using HogwartsPotions.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HogwartsPotions.Repositories
+{
+    public class StudentRepository : GenericRepository<Student>, IStudentRepository
+    {
+        public StudentRepository(HogwartsContext context) : base(context)
+        {
+        }
+
+        public async Task<Student?> AssignRoomTo(int id, int roomId)
+        {
+            Student? studentToBeAssignedTo = await GetAsync(id);
+            if (studentToBeAssignedTo == null)
+            {
+                return null;
+            }
+
+            studentToBeAssignedTo.RoomId = roomId;
+            await UpdateAsync(studentToBeAssignedTo);
+            return studentToBeAssignedTo;
+        }
+
+        public async Task<Student?> GetWithDetails(int id)
+        {
+            return await _context.Students.Include(q => q.Room)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(q => q.Id == id);
+        }
+    }
+}
