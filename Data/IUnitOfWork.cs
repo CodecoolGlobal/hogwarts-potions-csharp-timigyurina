127e2b6593427f483fa7a1378277609916027a22
timigyurina <timi.gyurina@gmail.com>
2023-01-22 10:20:25 +0100

    Implement Unit of Work pattern
    Create IUnitOfWork interface and an implementation.
    Use UnitOfWork to have access to all repositories and so that all Controllers will have to only know about the UnitOfWork, not the repositories themselves.
    This way, we can also ensure that all repositories use the same database context.
    
    Commit and Dispose methods are not used because I made a decision to do the savings (for create, update and delete) in the GenericRepository.
    The other way would be to use Save and Dispose methods in the Controller, calling the IUnitOfWork's Commit/CommitAsync method when creating, updating or deleting and use IUnitOfWork'S Dispose when we dispose the Controller itself (override method of MVC Controller type).
    
    More info: https://learn.microsoft.com/en-us/aspnet/mvc/overview/older-versions/getting-started-with-ef-5-using-mvc-4/implementing-the-repository-and-unit-of-work-patterns-in-an-asp-net-mvc-application,
    https://stackoverflow.com/questions/55239380/why-derive-from-controllerbase-vs-controller-for-asp-net-core-web-api,
    https://learn.microsoft.com/en-us/aspnet/core/web-api/?view=aspnetcore-7.0

---
+++ Data/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using HogwartsPotions.Repositories;
+
+namespace HogwartsPotions.Data
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        public IRoomRepository RoomRepository { get; }
+
+        int Commit();
+        Task<int> CommitAsync();
+        Task DisposeAsync();
+    }
+}
---
+++ Data/UnitOfWork.cs
@@ -0,0 +1,77 @@
+﻿using HogwartsPotions.Repositories;
+
+namespace HogwartsPotions.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly HogwartsContext _context;
+        private IRoomRepository _roomRepository;
+        private bool _disposed = false;
+
+        public UnitOfWork(HogwartsContext context, IRoomRepository roomRepository)
+        {
+            _context = context;
+            _roomRepository = roomRepository;
+        }
+
+
+        public IRoomRepository RoomRepository
+        {
+            get
+            {
+                //if (_roomRepository == null)
+                //{
+                //    _roomRepository = new RoomRepository(_context);
+                //}
+                return _roomRepository;
+            }
+        }
+
+        public int Commit()  // use this at adding, updating or deleting an entity
+        {
+            return _context.SaveChanges();
+        }
+
+        public Task<int> CommitAsync() // use this at adding, updating or deleting an entity async
+        {
+            return _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public async Task DisposeAsync()
+        {
+            await DisposeAsync(true);
+        }
+
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            _disposed = true;
+        }
+
+        protected async virtual Task DisposeAsync(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    await _context.DisposeAsync();
+                }
+            }
+            _disposed = true;
+        }
+
+    }
+}
