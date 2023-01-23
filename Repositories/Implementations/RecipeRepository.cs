8d5fef7330044a02c900ebee9a6005cc9163aebf
timigyurina <timi.gyurina@gmail.com>
2023-01-23 16:25:38 +0100

    add methods to help with creating a new Potion

--- Repositories/Implementations/RecipeRepository.cs
+++ Repositories/Implementations/RecipeRepository.cs
@@ -1,6 +1,7 @@
 ﻿using HogwartsPotions.Data;
 using HogwartsPotions.Models.Entities;
 using HogwartsPotions.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace HogwartsPotions.Repositories.Implementations
 {
@@ -8,6 +9,42 @@ namespace HogwartsPotions.Repositories.Implementations
     {
         public RecipeRepository(HogwartsContext context) : base(context)
         {
+        }
+
+        public async Task<Recipe?> CheckIfRecipeExistsWithIngredients(IEnumerable<Ingredient> ingredients)
+        {
+            IEnumerable<Recipe> detailedRecipesWithIngredientCount = _context.Recipes.Where(r => r.Consistencies.Count() == ingredients.Count())
+                .Include(r => r.Consistencies)
+                .AsNoTracking();
+
+            IEnumerable<int> idsOfIngredientsToCheck = ingredients.Select(i => i.Id);
+            //IEnumerable<Recipe> detailedRecipes = _context.Recipes.Include(r => r.Consistencies).AsNoTracking();
+
+            foreach (Recipe recipe in detailedRecipesWithIngredientCount)
+            {
+                IEnumerable<int> ingredientIdsInRecipe = recipe.Consistencies.Select(c => c.IngredientId);
+
+                bool areAllIngredientsInRecipe = idsOfIngredientsToCheck.Intersect(ingredientIdsInRecipe).Count() == idsOfIngredientsToCheck.Count();
+
+                if (areAllIngredientsInRecipe)
+                {
+                    Recipe? r = await GetAsync(recipe.Id);
+                    return r;
+                }
+
+            }
+            return null;
+        }
+
+        public async Task<Recipe?> CreateNewAsync(Student creator)
+        {
+            Recipe recipeToAdd = new Recipe()
+            {
+                Name = $"{creator.Name}'s recipe",
+                StudentId = creator.Id
+            };
+
+            return await AddAsync(recipeToAdd);
         }
     }
 }
--- Repositories/Interfaces/IRecipeRepository.cs
+++ Repositories/Interfaces/IRecipeRepository.cs
@@ -4,5 +4,7 @@ namespace HogwartsPotions.Repositories.Interfaces
 {
     public interface IRecipeRepository : IGenericRepository<Recipe>
     {
+       Task<Recipe?> CheckIfRecipeExistsWithIngredients(IEnumerable<Ingredient> ingredients);
+       Task<Recipe?> CreateNewAsync(Student creator);
     }
 }
