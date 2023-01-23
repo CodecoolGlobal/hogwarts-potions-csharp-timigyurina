30440a003e00070cd0cb0099c8868ae62e2710d8
timigyurina <timi.gyurina@gmail.com>
2023-01-23 16:24:38 +0100

    create DbInitialiser and seed data with Ingredients

---
+++ Data/DbInitialiser.cs
@@ -0,0 +1,38 @@
+﻿using HogwartsPotions.Models.Entities;
+
+namespace HogwartsPotions.Data
+{
+    public static class DbInitialiser
+    {
+        public static void Initialise(HogwartsContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Ingredients.Any())
+            {
+                return;   
+            }
+
+            Ingredient[] ingredients = new Ingredient[]
+            {
+                new Ingredient(){ Name = "water" },
+                new Ingredient(){ Name = "earth" },
+                new Ingredient(){ Name = "salt" },
+                new Ingredient(){ Name = "mud" },
+                new Ingredient(){ Name = "grass" },
+                new Ingredient(){ Name = "oakleaf" },
+                new Ingredient(){ Name = "gold" },
+                new Ingredient(){ Name = "ash" },
+                new Ingredient(){ Name = "pumpkin" },
+                new Ingredient(){ Name = "apple" }
+            };
+
+
+            foreach (Ingredient ingredient in ingredients)
+            {
+                context.Ingredients.Add(ingredient);
+            }
+            context.SaveChanges();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using HogwartsPotions.Data;
 
 namespace HogwartsPotions
 {
@@ -7,9 +6,30 @@ namespace HogwartsPotions
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            CreateDbIfNotExists(host);
+
+            host.Run();
         }
 
+        private static void CreateDbIfNotExists(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<HogwartsContext>();
+                    DbInitialiser.Initialise(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
+        }
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
