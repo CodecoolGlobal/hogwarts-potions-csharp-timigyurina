7a83a4039ebb5796bdbbb1cda5cf94a7a47c7510
timigyurina <timi.gyurina@gmail.com>
2023-01-23 20:22:10 +0100

    add seed data for Rooms

--- Data/DbInitialiser.cs
+++ Data/DbInitialiser.cs
@@ -1,4 +1,5 @@
 ﻿using HogwartsPotions.Models.Entities;
+using HogwartsPotions.Models.Enums;
 
 namespace HogwartsPotions.Data
 {
@@ -8,31 +9,50 @@ namespace HogwartsPotions.Data
         {
             context.Database.EnsureCreated();
 
-            if (context.Ingredients.Any())
+            if (!context.Ingredients.Any())
             {
-                return;   
+                Ingredient[] ingredients = new Ingredient[]
+                {
+                    new Ingredient(){ Name = "water" },
+                    new Ingredient(){ Name = "earth" },
+                    new Ingredient(){ Name = "salt" },
+                    new Ingredient(){ Name = "mud" },
+                    new Ingredient(){ Name = "grass" },
+                    new Ingredient(){ Name = "oakleaf" },
+                    new Ingredient(){ Name = "gold" },
+                    new Ingredient(){ Name = "ash" },
+                    new Ingredient(){ Name = "pumpkin" },
+                    new Ingredient(){ Name = "apple" }
+                };
+
+
+                foreach (Ingredient ingredient in ingredients)
+                {
+                    context.Ingredients.Add(ingredient);
+                }
+                context.SaveChanges();
+
             }
 
-            Ingredient[] ingredients = new Ingredient[]
+            if (!context.Rooms.Any())
             {
-                new Ingredient(){ Name = "water" },
-                new Ingredient(){ Name = "earth" },
-                new Ingredient(){ Name = "salt" },
-                new Ingredient(){ Name = "mud" },
-                new Ingredient(){ Name = "grass" },
-                new Ingredient(){ Name = "oakleaf" },
-                new Ingredient(){ Name = "gold" },
-                new Ingredient(){ Name = "ash" },
-                new Ingredient(){ Name = "pumpkin" },
-                new Ingredient(){ Name = "apple" }
-            };
+                Room[] rooms = new Room[]
+                {
+                    new Room(){ House = HouseType.Gryffindor, Capacity = 2 },
+                    new Room(){ House = HouseType.Slytherin, Capacity = 2 },
+                    new Room(){ House = HouseType.Hufflepuff, Capacity = 2 },
+                    new Room(){ House = HouseType.Ravenclaw, Capacity = 2 },
+                    new Room(){ House = HouseType.Gryffindor, Capacity = 5 },
+                    new Room(){ House = HouseType.Slytherin, Capacity = 5 },
+                    new Room(){ House = HouseType.Ravenclaw, Capacity = 5 },
+                };
 
-
-            foreach (Ingredient ingredient in ingredients)
-            {
-                context.Ingredients.Add(ingredient);
+                foreach (Room room in rooms)
+                {
+                    context.Rooms.Add(room);    
+                }
+                context.SaveChanges();
             }
-            context.SaveChanges();
         }
     }
 }
